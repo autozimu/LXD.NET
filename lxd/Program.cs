9d1b294e348f3300d603773e04f9c57b53418bd8
Jeff Li <junfengl@ca.ibm.com>
2016-07-28 13:57:02 -0400

    Fix certificate.
    - X509Certificate2, not X509Certificate.
    - p12, not crt.

--- lxd/Program.cs
+++ lxd/Program.cs
@@ -14,20 +14,20 @@ namespace lxd
     {
         static void Main(string[] args)
         {
-            // Bypass handshake error. C# default tls13 is not supported by lxd.
-            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11;
-            // Bypass self-signed certificate error.
-            ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyError) => true;
+			// Bypass handshake error. C# default tls13 is not supported by lxd.
+			ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11;
+			// Bypass self-signed certificate error.
+			ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyError) => true;
             // Bypass slow proxy auto configuration.
             WebRequest.DefaultWebProxy = null;
 
-            RestClient client = new RestClient("https://ubuntu.local:8443");
+            RestClient client = new RestClient("https://ubuntu:8443");
 
-            X509Certificate cert = X509Certificate.CreateFromCertFile(@"cert/client.crt");
-            client.ClientCertificates = new X509CertificateCollection();
-            client.ClientCertificates.Add(cert);
+			// Add client certificate.
+			client.ClientCertificates = new X509CertificateCollection();
+			client.ClientCertificates.Add(new X509Certificate2("cert/client.p12"));
 
-            RestRequest request = new RestRequest("/1.0");
+			RestRequest request = new RestRequest("/1.0");
 
             IRestResponse response = client.Execute(request);
 
--- test.py
+++ test.py
@@ -3,8 +3,8 @@
 import os.path
 import requests
 
-conf_dir = os.path.expanduser('~/.config/lxc')
+conf_dir = os.path.expanduser('~/repo/lxd.NET/lxd/cert')
 crt = os.path.join(conf_dir, 'client.crt')
 key = os.path.join(conf_dir, 'client.key')
 
-print(requests.get('https://127.0.0.1:8443/1.0', verify=False, cert=(crt, key)).text)
+print(requests.get('https://ubuntu:8443/1.0', verify=False, cert=(crt, key)).text)
