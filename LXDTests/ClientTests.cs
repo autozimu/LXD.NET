fc838ae88ce043193eec5de7cbe668ff0c690958
Junfeng (Jeff) Li <autozimu@gmail.com>
2016-08-28 23:29:20 -0400

    Start tests.

---
+++ LXDTests/ClientTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LXD;
+using LXD.Domain;
+
+namespace LXDTests
+{
+    [TestClass]
+    public class ClientTests
+    {
+        [TestMethod]
+        public void Constructor()
+        {
+            Client client = new Client("https://ubuntu:8443", "cert/client.p12", "");
+
+            Assert.AreEqual(true, client.Trusted);
+        }
+    }
+}
